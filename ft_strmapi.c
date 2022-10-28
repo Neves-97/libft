cd7d9fa7d05e6dd89a04469b6a4c5cc2bdbffd50
Rodrigo Da Mina Neves <roda-min@c1r5s5.42lisboa.com>
2022-10-28 19:44:05 +0100

    m

---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+** Applies the function f to each character of the string passed
+** as argument by giving its index as first argument to create a
+** “fresh” new string (with malloc(3)) resulting from the successive
+** applications of f.
+** Param. #1 The string to map.
+** Param. #2 The function to apply to each character of s and its index.
+** Return value The “fresh” string created from the successive applications of
+** f.
+** Libc functions malloc(3)
+*/
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+
+}
