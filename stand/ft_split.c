551291a1af6d69592bda04d61516d4c5499095e5
Neves-97 <damianeves@gmail.com>
2022-11-15 18:23:56 +0000

    .

--- stand/ft_split.c
+++ stand/ft_split.c
@@ -6,9 +6,11 @@
 /*   By: roda-min <roda-min@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:41:27 by roda-min          #+#    #+#             */
-/*   Updated: 2022/11/14 20:08:40 by roda-min         ###   ########.fr       */
+/*   Updated: 2022/11/15 18:20:13 by roda-min         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//Conaca da tia	split = ["Conaca", "da", "tia", NULL]	split[0] = "Conaca" 26
 
 #include "libft.h"
 
@@ -33,6 +35,8 @@ static int	count_words(const char *str, char c)
 	return (i);
 }
 
+//start == 3 finish == 9 -> 9 - 3 + 1 ||40
+
 static char	*word_dup(const char *str, int start, int finish)
 {
 	char	*word;
@@ -40,11 +44,15 @@ static char	*word_dup(const char *str, int start, int finish)
 
 	i = 0;
 	word = malloc((finish - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
 	while (start < finish)
 		word[i++] = str[start++];
 	word[i] = '\0';
 	return (word);
 }
+
+//   Conaca da tia	index == 3 || 55
 
 char	**ft_split(char const *s, char c)
 {
@@ -70,91 +78,6 @@ char	**ft_split(char const *s, char c)
 		}
 		i++;
 	}
-	split[j] = 0;
+	split[j] = NULL;
 	return (split);
 }
-// static int	numstring(char const *s1, char c)
-// {
-// 	int	comp;
-// 	int	cles;
-
-// 	comp = 0;
-// 	cles = 0;
-// 	if (*s1 == '\0')
-// 		return (0);
-// 	while (*s1 != '\0')
-// 	{
-// 		if (*s1 == c)
-// 			cles = 0;
-// 		else if (cles == 0)
-// 		{
-// 			cles = 1;
-// 			comp++;
-// 		}
-// 		s1++;
-// 	}
-// 	return (comp);
-// }
-
-// static int	numchar(char const *s2, char c, int i)
-// {
-// 	int	lenght;
-
-// 	lenght = 0;
-// 	while (s2[i] != c && s2[i] != '\0')
-// 	{
-// 		lenght++;
-// 		i++;
-// 	}
-// 	return (lenght);
-// }
-
-// char	**freee(char **dst, int j)
-// {
-// 	while (j > 0)
-// 	{
-// 		j--;
-// 		free((void *)dst[j]);
-// 	}
-// 	free(dst);
-// 	return (NULL);
-// }
-
-// static char	**affect(char const *s, char **dst, char c, int l)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	k;
-
-// 	i = 0;
-// 	j = 0;
-// 	while (s[i] != '\0' && j < l)
-// 	{
-// 		k = 0;
-// 		while (s[i] == c)
-// 			i++;
-// 		dst[j] = (char *)malloc(sizeof(char) * numchar(s, c, i) + 1);
-// 		if (dst[j] == NULL)
-// 			return (freee(dst, j));
-// 		while (s[i] != '\0' && s[i] != c)
-// 			dst[j][k++] = s[i++];
-// 		dst[j][k] = '\0';
-// 		j++;
-// 	}
-// 	dst[j] = 0;
-// 	return (dst);
-// }
-
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**dst;
-// 	int		l;
-
-// 	if (s == NULL)
-// 		return (NULL);
-// 	l = numstring(s, c);
-// 	dst = (char **)malloc(sizeof(char *) * (l + 1));
-// 	if (dst == NULL)
-// 		return (NULL);
-// 	return (affect(s, dst, c, l));
-// }
