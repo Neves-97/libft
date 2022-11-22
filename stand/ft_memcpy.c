51078dff7b2f5083c2015d84f4a80062d9b70647
Neves-97 <damianeves@gmail.com>
2022-11-22 14:53:06 +0000

    .

--- stand/ft_memcpy.c
+++ stand/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: roda-min <roda-min@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:41:04 by roda-min          #+#    #+#             */
-/*   Updated: 2022/11/08 14:32:20 by roda-min         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:52:59 by roda-min         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,6 @@
 
 // The C library function void *memcpy(void *dest, const void *src, size_t n) 
 // copies n characters from memory area src to memory area dest.
-
-// void	*ft_memcpy(void *dest, const void *src, size_t n)
-// {
-// 	size_t	i;
-// 	char	*s1;
-
-// 	s1 = (char *)dest;
-// 	i = 0;
-// 	while (i < n)
-// 	{
-// 		*(char *)s1 = *(char *)src;
-// 		s1++;
-// 		src++;
-// 		i++;
-// 	}
-// 	return (dest);
-// }
 
 void	*ft_memcpy(void *str1, const void *str2, size_t n)
 {
